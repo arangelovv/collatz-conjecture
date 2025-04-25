96c9c670fe39edd6de7164e45a66237fcf18759d
rangelovvjobs <rangelovvjobs@gmail.com>
2025-04-25 12:05:10 +0300

    implement collatz algorithm

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
-package main 
+package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("Collatz Conjecture")
+func collatz(n int) int {
+	if n%2 == 0 {
+		n = n / 2
+		fmt.Println(n)
+		return n
+	} else {
+		n = (n * 3) + 1
+		fmt.Println(n)
+		return n
+	}
 }
+
+func main() {
+	fmt.Println("Collatz Conjecture")
+
+	var input, steps int
+
+	fmt.Print("Input any positive number:")
+	fmt.Scan(&input)
+
+	//main collatz loop
+	for i := 0; input != 1; i++ {
+		input = collatz(input)
+		steps = steps + 1
+		if input == 1 {
+			fmt.Println("steps count:", steps)
+		}
+	}
+
+}
+
